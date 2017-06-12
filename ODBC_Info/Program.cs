70e1e07864b9c0cbc8b4ccbec4174e20603adee7
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-06-12 12:57:47 -0400

    init
    This app gets the information of odbc drivers installed in the computer

---
+++ ODBC_Info/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ODBCInfoGetterPluggin;
+namespace ODBC_Info
+{
+    class Program
+    {
+        GetInfo _gi = new GetInfo();
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.ShowDrivers();
+            Console.Read();
+        }
+        private void ShowDrivers()
+        {
+            var drivers = _gi.GetSystemDriverList();
+            foreach (var a in drivers)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
